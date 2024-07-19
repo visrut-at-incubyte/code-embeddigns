687423349e73d161226f6fee22f490c08c05ef66
visrut <visrut@incubyte.co>
2024-07-19 20:07:15 +0530

    scan with token count

---
+++ README.md
@@ -0,0 +1,8 @@
+- Token Limit for OpenAI Embeddings models
+- https://platform.openai.com/docs/guides/embeddings
+
+| Model                  | ~ Pages per dollar | Performance on MTEB eval | Max input |
+| ---------------------- | ------------------ | ------------------------ | --------- |
+| text-embedding-3-small | 62,500             | 62.3%                    | 8191      |
+| text-embedding-3-large | 9,615              | 64.6%                    | 8191      |
+| text-embedding-ada-002 | 12,500             | 61.0%                    | 8191      |
--- src/scan.ts
+++ src/scan.ts
@@ -1,9 +1,18 @@
 import { glob } from "glob";
+import * as path from "path";
+import * as fs from "fs/promises";
 import * as dotenv from "dotenv";
+import { encode } from "gpt-tokenizer";
 
 dotenv.config();
 
-const scanDirectory = async (dir: string, ignorePatterns: string[] = []) => {
+interface CodeFile {
+  path: string;
+  source_code: string;
+  token_count: number;
+}
+
+const scan_directory = async (dir: string, ignorePatterns: string[] = []) => {
   const paths = await glob("**/*", {
     ignore: ignorePatterns,
     cwd: dir,
@@ -13,14 +22,33 @@ const scanDirectory = async (dir: string, ignorePatterns: string[] = []) => {
   return paths.filter((path) => path.isFile()).map((file) => file.fullpath());
 };
 
-const ignorePatterns = process.env.IGNORE_PATTERNS?.split(",") || [];
+const ignore_patterns = process.env.IGNORE_PATTERNS?.split(",") || [];
+
+const get_code_files_within_token_limit = async (
+  file_paths: string[]
+): Promise<CodeFile[]> => {
+  const code_files: CodeFile[] = [];
+
+  for (const file_path of file_paths) {
+    const source_code = await fs.readFile(file_path, "utf-8");
+    const token_count = encode(source_code).length;
+    if (token_count <= parseInt(process.env.TOKEN_LIMIT!)) {
+      code_files.push({ path: file_path, source_code, token_count });
+    }
+  }
+
+  return code_files;
+};
 
 const scan = async () => {
-  const files = await scanDirectory(process.cwd(), ignorePatterns);
-  console.log(files.length);
-  // for (const file of files) {
-  //   console.log(file);
-  // }
+  const file_paths = await scan_directory(
+    path.join(process.cwd(), process.env.CODEBASE_PATH ?? ""),
+    ignore_patterns
+  );
+
+  const code_files = await get_code_files_within_token_limit(file_paths);
+  console.log(code_files[0]);
+  console.log(code_files.length);
 };
 
 scan();
