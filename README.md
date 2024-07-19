a3f9a475b653ee1d71dbff61ca18ab491acb1341
visrut <visrut@incubyte.co>
2024-07-19 20:55:53 +0530

    add qdrant

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 | text-embedding-3-small | 62,500             | 62.3%                    | 8191      |
 | text-embedding-3-large | 9,615              | 64.6%                    | 8191      |
 | text-embedding-ada-002 | 12,500             | 61.0%                    | 8191      |
+
+- You have to run this
+- docker run -p 6333:6333 --name code_similarity_qdrant -d qdrant/qdrant
--- src/scan.ts
+++ src/scan.ts
@@ -3,27 +3,24 @@ import * as path from "path";
 import * as fs from "fs/promises";
 import * as dotenv from "dotenv";
 import { encode } from "gpt-tokenizer";
-
-dotenv.config();
+import { QdrantClient } from "@qdrant/js-client-rest";
 
 interface CodeFile {
   path: string;
   source_code: string;
+  base_name: string;
   token_count: number;
+  vector?: number[];
 }
+
+dotenv.config();
+
+const vec_db = new QdrantClient({
+  url: "http://localhost:6333",
+});
 
 const cache_dir = path.join(process.cwd(), "cache");
 fs.mkdir(cache_dir, { recursive: true });
-
-const scan_directory = async (dir: string, ignorePatterns: string[] = []) => {
-  const paths = await glob("**/*", {
-    ignore: ignorePatterns,
-    cwd: dir,
-    withFileTypes: true,
-  });
-
-  return paths.filter((path) => path.isFile()).map((file) => file.fullpath());
-};
 
 const ignore_patterns = process.env.IGNORE_PATTERNS?.split(",") || [];
 
@@ -44,7 +41,12 @@ const get_code_files_within_token_limit = async (
       const token_count = encode(source_code).length;
 
       if (token_count <= parseInt(process.env.TOKEN_LIMIT!)) {
-        code_file = { path: file_path, source_code, token_count };
+        code_file = {
+          source_code,
+          token_count,
+          path: file_path,
+          base_name: path.basename(file_path),
+        };
         await fs.writeFile(cache_path, JSON.stringify(code_file), "utf-8");
       }
     }
@@ -64,8 +66,44 @@ const scan = async () => {
   );
 
   const code_files = await get_code_files_within_token_limit(file_paths);
-  console.log(code_files[0]);
-  console.log(code_files.length);
+
+  const chunks = chunk_array(code_files, 10);
+  chunks.forEach((chunk, chunkIndex) => {
+    const ids = chunk.map((_, index) => chunkIndex * 10 + index + 1);
+    const payloads = chunk.map((file) => ({
+      path: file.path,
+      source_code: file.source_code,
+      base_name: file.base_name,
+      token_count: file.token_count,
+    }));
+    const vectors = chunk.map((file) => file.vector || []);
+
+    vec_db.upsert("codebase", {
+      batch: {
+        ids: ids,
+        payloads: payloads,
+        vectors: vectors,
+      },
+    });
+  });
 };
 
 scan();
+
+const scan_directory = async (dir: string, ignorePatterns: string[] = []) => {
+  const paths = await glob("**/*", {
+    ignore: ignorePatterns,
+    cwd: dir,
+    withFileTypes: true,
+  });
+
+  return paths.filter((path) => path.isFile()).map((file) => file.fullpath());
+};
+
+const chunk_array = <T>(array: T[], size: number): T[][] => {
+  const result: T[][] = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+};
